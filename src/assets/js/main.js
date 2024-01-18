443c79558de44d1d6dc33211e2177a9dcd9d01a1
Arif Hossin || Full Stack Developer <46327525+webdevarif@users.noreply.github.com>
2024-01-18 09:19:10 +0600

    Update

---
+++ src/assets/js/main.js
@@ -0,0 +1,105 @@
+(function ($) {
+    const THEME = {
+      init() {
+        this.scrollbar();
+        this.setupUlSelect();
+        this.setupTAB();
+      },
+  
+      scrollbar() {  
+        Scrollbar.initAll();
+      },
+  
+      setupUlSelect() {
+        $('.ul-select-input').on('click', function () {
+          const $menu = $(this).closest('.ul-select');
+  
+          const onMouseUp = (e) => {
+            if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
+              $menu.removeClass('is-active').find('.ul-select-list').slideUp(100);
+            }
+          };
+  
+          $menu.toggleClass('is-active')
+            .find('.ul-select-list')
+            .slideDown(100)
+            .promise()
+            .done(() => {
+              if ($menu.hasClass('is-active')) {
+                $(document).on('mouseup', onMouseUp);
+              } else {
+                $(document).off('mouseup', onMouseUp);
+              }
+            });
+        });
+  
+        // CHOOSE INPUT SELECT
+        $('.ul-select-list li[data-value]').on('click', function (event) {
+          event.preventDefault();
+          const $that = $(this);
+          const $menu = $that.closest('.ul-select');
+          const $name = $menu.find('.ul-select-name');
+          const $input = $menu.find('.input-hidden-field');
+  
+          $menu.removeClass('is-active')
+            .find('.ul-select-list')
+            .slideUp(100, () => {
+              $name.html($that.html());
+              $input.val($that.data('value'));
+              $menu.removeClass('no-selected').addClass('selected');
+            });
+        });
+  
+        // CHOOSE INPUT SELECT
+        $('.select-arrow-cancel').on('click', function (event) {
+          event.preventDefault();
+          const $that = $(this);
+          const $menu = $that.closest('.ul-select');
+          const $name = $menu.find('.ul-select-name');
+          const $input = $menu.find('.input-hidden-field');
+  
+          $menu.removeClass('is-active')
+            .find('.ul-select-list')
+            .slideUp(200, () => {
+              $input.val('');
+              $name.html('');
+              $menu.addClass('no-selected').removeClass('selected');
+            });
+        });
+      },
+
+      setupTAB(){
+        $(document).ready(function() {
+          // Handle tab clicks
+          $('.tab-link').on('click', function(e) {
+              e.preventDefault();
+
+              // Hide all tab content
+              $('.tab-panel').removeClass('active');
+
+              // Show the selected tab content
+              var targetTab = $(this).attr('href');
+              $('.tab-panel' + targetTab ).addClass('active');
+
+              // Update the active class on the tab navigation
+              $('.tab-link').parent().removeClass('active');
+              $(this).parent().addClass('active');
+          });
+      });
+      }
+
+
+    };
+  
+    $(document).ready(function () {
+      THEME.init();    
+    });
+  
+  })(jQuery);
+
+
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
       // 'display': ['Oswald', ...],
       'primary': ['var(--ff-primary)'],
     },
-    extend: {},
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      DEFAULT: '12px',
+      'md': '0.375rem',
+      'lg': '0.5rem',
+      'full': '9999px',
+      'large': '12px',
+    },
+    extend: {
+      colors: {
+        "primary": "#f30292",
+        "primary-50": "#9a6c88",
+        "primary-dark": "#331630",
+        "secondary": "#65265E",
+      }
+    },
   },
   plugins: [],
 }
