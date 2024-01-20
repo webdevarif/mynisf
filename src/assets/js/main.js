3bf9d9cdeb3209fdd1e69f4f1624e245c9bca122
Arif Hossin || Full Stack Developer <46327525+webdevarif@users.noreply.github.com>
2024-01-20 18:23:55 +0600

    Update

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,6 +5,7 @@
         this.setupUlSelect();
         this.setupTAB();
         this.setupDropdown();
+        this.setupThemeSLIDER();
       },
   
       scrollbar() {  
@@ -117,6 +118,15 @@
               });
           });
         });
+      },
+
+      setupThemeSLIDER(){
+        if($('[theme-slider]').length){
+          $('[theme-slider]').slick({
+            prevArrow:'<button type="button" class="arrows-left"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"></path></svg></button>',
+            nextArrow:'<button type="button" class="arrows-right"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 320 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"></path></svg></button>'
+          });
+        }
       }
 
 
